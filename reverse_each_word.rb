277dbd2875fb37018e03f8583c6162efa17994bb
Alex S Grimes <alex@alexgrimes.com>
2020-04-29 16:22:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,22 +1,22 @@
-require "pry"
-def reverse_each_word(string)
-  split_string = string.split("")
-  reversed = []
-  split_string.each{|element| element.reverse split_string << element.reverse }
-  binding.pry
-  reversed.join
-end
+# require "pry"
+# def reverse_each_word(string)
+#   split_string = string.split("")
+#   reversed = []
+#   split_string.each{|element| element.reverse split_string << element.reverse }
+#   binding.pry
+#   reversed.join
+# end
+
+# def reverse_each_word(string)
+#   split_string = string.split("")
+#   reversed = []
+#   split_string.each{|element| element.reverse reversed << split_string }
+#   reversed.join
+# end
 
 def reverse_each_word(string)
   split_string = string.split("")
   reversed = []
-  split_string.each{|element| element.reverse reversed << split_string }
-  reversed.join
-end
-
-def reverse_each_word(string)
-  split_string = string.split("")
-  reversed = []
-  split_string.each{|element| element.reverse reversed << split_string }
+  reversed.each{|element| element.reverse reversed << element.reversed }
   reversed.join
 end
