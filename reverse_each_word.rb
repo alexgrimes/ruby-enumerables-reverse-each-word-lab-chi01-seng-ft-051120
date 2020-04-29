fd06500330dae0a0e3e7d37a24a9b650eb289847
Alex S Grimes <alex@alexgrimes.com>
2020-04-29 16:06:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 def reverse_each_word(string)
   split_string = string.split("")
   reversed = []
-  split_string.each{|element| reversed << element.reverse }
+  split_string.each{|element| split_string << element.reverse }
   binding.pry
   reversed.join
 end
